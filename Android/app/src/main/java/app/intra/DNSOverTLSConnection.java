2c0eec5318644ec8d4f22cb7d5e7142121421c31
Daniel Stirnimann <daniel.stirnimann@switch.ch>
2018-08-09 11:32:10 +0200

    Add TLS hostname verification

--- Android/app/src/main/java/app/intra/DNSOverTLSConnection.java
+++ Android/app/src/main/java/app/intra/DNSOverTLSConnection.java
@@ -2,17 +2,17 @@ package app.intra;
 
 import android.net.VpnService;
 import android.util.Log;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
-
-import java.net.Socket;
 import java.net.UnknownHostException;
-
-
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLHandshakeException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
 
 import app.intra.util.DnsMetadata;
 import okhttp3.Callback;
@@ -56,12 +56,27 @@ public class DNSOverTLSConnection implements ServerConnection {
                 try {
                     Log.d(TAG, "Trying to perform DNS-over-TLS lookup via " + serverIP);
                     long start = System.currentTimeMillis();
-                    Socket dnsSocket;
+                    SSLSocket dnsSocket;
 
+                    // TODO: Handle TLS connection errors e.g. refused
+                    // TODO: Allow self-signed certificate, maybe allow hostname mismatch
                     SSLContext context = SSLContext.getInstance("TLSv1.2");
                     context.init(null, null, null);
-                    dnsSocket = context.getSocketFactory()
+                    dnsSocket = (SSLSocket) context.getSocketFactory()
                             .createSocket(serverIP, DoTPORT);
+
+                    HostnameVerifier hv = HttpsURLConnection.getDefaultHostnameVerifier();
+                    SSLSession s = dnsSocket.getSession();
+
+                    // Verify the certicate hostname
+                    // This is due to lack of SNI support in the current SSLSocket.
+                    if (!hv.verify(hostname, s)) {
+                        throw new SSLHandshakeException("Expected " + hostname + ", " +
+                                "found " + s.getPeerPrincipal());
+                    }
+
+                    // At this point SSLSocket performed certificate verification and
+                    // we have performed hostname verification, so it is safe to proceed.
 
                     vpn.protect(dnsSocket);
 
